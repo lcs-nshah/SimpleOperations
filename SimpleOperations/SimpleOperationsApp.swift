5044914f11d304772aaf87712f428f09a5af5a81
Neil Shah <nshah@lcs.on.ca>
2025-02-12 09:07:47 -0500

    Initial Commit

---
+++ SimpleOperations/SimpleOperationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleOperationsApp.swift
+//  SimpleOperations
+//
+//  Created by Neil Shah on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct SimpleOperationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
