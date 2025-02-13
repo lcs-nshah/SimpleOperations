61a68ada294ae6e5b095b9e0d264d1577d8d776d
Neil Shah <nshah@lcs.on.ca>
2025-02-13 18:55:46 -0500

    Added variables and a computed property to get the quotient from these variables in the Division View.

--- SimpleOperations/Views/DivisionView.swift
+++ SimpleOperations/Views/DivisionView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct DivisionView: View {
+    
+    // MARK: Stored Properties
+    @State var dividend: Int = 1
+    @State var divisor: Int = 1
+    
+    // MARK: Computed Properties
+    var quotient: Int {
+        return dividend / divisor
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
